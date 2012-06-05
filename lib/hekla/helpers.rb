bceed12822d90de47aa224ec9e90f1b9deeb18b3
Brandur <brandur@mutelight.org>
2012-06-05 13:16:02 +0200

    Fix for block links

--- lib/hekla/helpers.rb
+++ lib/hekla/helpers.rb
@@ -46,11 +46,16 @@ module Hekla
       end
     end
 
-    def link_to(title, uri, attrs = {})
+    # @todo: OMG HOLY SHIT FUGLY
+    def link_to(*args)
+      attrs = args.last.is_a?(Hash) ? args.pop : {}
+      title = args[1] ? args[0] : nil
+      uri   = args[1] ? args[1] : args[0]
       uri = uri.to_path if uri.kind_of?(ActiveRecord::Base)
+      attrs[:title] = title if title
       attr_str = attrs.map { |k, v| %{#{k}="#{v}"} }.join(" ")
       attr_str = " #{attr_str}" if attr_str.length > 0
-      %{<a href="#{uri}" title="#{title}"#{attr_str}>#{title}</a>}
+      %{<a href="#{uri}" #{attr_str}>#{block_given? ? yield : title}</a>}
     end
 
     def pjax?
