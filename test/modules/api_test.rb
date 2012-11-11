0091b13c47fc8675f77c100426626473deab0d87
Brandur <brandur@mutelight.org>
2012-11-10 19:07:53 -0800

    Modularize test suites

---
+++ test/modules/api_test.rb
@@ -0,0 +1,109 @@
+require_relative "../test_helper"
+
+describe Hekla::Modules::API do
+  include Rack::Test::Methods
+
+  def app
+    Hekla::Main
+  end
+
+  let(:article)          { Article.new(valid_attributes) }
+  let(:cache)            { CacheStub.new }
+  let(:valid_attributes) {
+    { title:        "About",
+      slug:         "about",
+      summary:      "About the Surf.",
+      content:      "About the Surf.",
+      published_at: Time.now.iso8601 }
+  }
+
+  before do
+    stub(Dalli::Client).new { cache }
+  end
+
+  describe "POST /articles" do
+    it "requires authorization" do
+      post "/articles", { article: valid_attributes.to_json }
+      last_response.status.must_equal 401
+      last_response.body.parse_json["message"].must_equal "Not authorized"
+    end
+
+    it "creates an article" do
+      authorize "", "KEY"
+      post "/articles", { article: valid_attributes.to_json }
+      last_response.status.must_equal 201
+    end
+
+    it "creates an article with file push" do
+      authorize "", "KEY"
+      time_str = "Time.parse(#{valid_attributes[:published_at]})"
+      post "/articles", {
+        attributes: valid_attributes.slice(:title, :slug, :summary).
+          merge(published_at: time_str).to_s,
+        content: valid_attributes[:content] }
+      last_response.status.must_equal 201
+    end
+
+    it "fails to create an article" do
+      authorize "", "KEY"
+      mock(article).save { raise(Sequel::ValidationFailed.new([])) }
+      mock(Article).new.with_any_args { article }
+      post "/articles", { article: valid_attributes.to_json }
+      last_response.status.must_equal 422
+      last_response.body.parse_json.wont_equal nil
+    end
+  end
+
+  describe "PUT /articles/:id" do
+    before { article.save }
+
+    it "requires authorization" do
+      put "/articles/about", { article: valid_attributes.to_json }
+      last_response.status.must_equal 401
+      last_response.body.parse_json["message"].must_equal "Not authorized"
+    end
+
+    it "updates an article" do
+      authorize "", "KEY"
+      put "/articles/about", { article: valid_attributes.to_json }
+      last_response.status.must_equal 200
+    end
+
+    it "updates an article with file push" do
+      authorize "", "KEY"
+      time_str = "Time.parse(#{valid_attributes[:published_at]})"
+      put "/articles/about", {
+        attributes: valid_attributes.slice(:title, :summary).
+          merge(published_at: time_str).to_s,
+        content: valid_attributes[:content] }
+      last_response.status.must_equal 200
+    end
+
+    it "fails to update an article" do
+      authorize "", "KEY"
+      mock(Article).find_by_slug!("about") { article }
+      mock(article).update.with_any_args {
+        raise(Sequel::ValidationFailed.new([]))
+      }
+      put "/articles/about", { article: valid_attributes.to_json }
+      last_response.status.must_equal 422
+      last_response.body.parse_json.wont_equal nil
+    end
+  end
+
+  describe "DELETE /articles/:id" do
+    before { article.save }
+
+    it "requires authorization" do
+      delete "/articles/about"
+      last_response.status.must_equal 401
+      last_response.body.parse_json["message"].must_equal "Not authorized"
+    end
+
+    it "deletes an article" do
+      authorize "", "KEY"
+      delete "/articles/about"
+      last_response.status.must_equal 200
+    end
+  end
+end
--- test/modules/web_test.rb
+++ test/modules/web_test.rb
@@ -1,9 +1,12 @@
-require_relative "./test_helper"
+require_relative "../test_helper"
 
-describe Hekla do
+describe Hekla::Modules::Web do
   include Rack::Test::Methods
 
-  let(:app)              { Hekla::Main }
+  def app
+    Hekla::Main
+  end
+
   let(:article)          { Article.new(valid_attributes) }
   let(:cache)            { CacheStub.new }
   let(:valid_attributes) {
@@ -19,7 +22,6 @@ describe Hekla do
   end
 
   it "responds with a 404" do
-    set :show_exceptions, false
     get "/does-not-exist"
     last_response.status.must_equal 404
   end
@@ -138,92 +140,6 @@ describe Hekla do
     it "responds with 404 for a non-existent tiny slug" do
       get "/a/8"
       last_response.status.must_equal 404
-    end
-  end
-
-  describe "POST /articles" do
-    it "requires authorization" do
-      post "/articles", { article: valid_attributes.to_json }
-      last_response.status.must_equal 401
-      last_response.body.parse_json["message"].must_equal "Not authorized"
-    end
-
-    it "creates an article" do
-      authorize "", "KEY"
-      post "/articles", { article: valid_attributes.to_json }
-      last_response.status.must_equal 201
-    end
-
-    it "creates an article with file push" do
-      authorize "", "KEY"
-      time_str = "Time.parse(#{valid_attributes[:published_at]})"
-      post "/articles", {
-        attributes: valid_attributes.slice(:title, :slug, :summary).
-          merge(published_at: time_str).to_s,
-        content: valid_attributes[:content] }
-      last_response.status.must_equal 201
-    end
-
-    it "fails to create an article" do
-      authorize "", "KEY"
-      mock(article).save { raise(Sequel::ValidationFailed.new([])) }
-      mock(Article).new.with_any_args { article }
-      post "/articles", { article: valid_attributes.to_json }
-      last_response.status.must_equal 422
-      last_response.body.parse_json.wont_equal nil
-    end
-  end
-
-  describe "PUT /articles/:id" do
-    before { article.save }
-
-    it "requires authorization" do
-      put "/articles/about", { article: valid_attributes.to_json }
-      last_response.status.must_equal 401
-      last_response.body.parse_json["message"].must_equal "Not authorized"
-    end
-
-    it "updates an article" do
-      authorize "", "KEY"
-      put "/articles/about", { article: valid_attributes.to_json }
-      last_response.status.must_equal 200
-    end
-
-    it "updates an article with file push" do
-      authorize "", "KEY"
-      time_str = "Time.parse(#{valid_attributes[:published_at]})"
-      put "/articles/about", {
-        attributes: valid_attributes.slice(:title, :summary).
-          merge(published_at: time_str).to_s,
-        content: valid_attributes[:content] }
-      last_response.status.must_equal 200
-    end
-
-    it "fails to update an article" do
-      authorize "", "KEY"
-      mock(Article).find_by_slug!("about") { article }
-      mock(article).update.with_any_args {
-        raise(Sequel::ValidationFailed.new([]))
-      }
-      put "/articles/about", { article: valid_attributes.to_json }
-      last_response.status.must_equal 422
-      last_response.body.parse_json.wont_equal nil
-    end
-  end
-
-  describe "DELETE /articles/:id" do
-    before { article.save }
-
-    it "requires authorization" do
-      delete "/articles/about"
-      last_response.status.must_equal 401
-      last_response.body.parse_json["message"].must_equal "Not authorized"
-    end
-
-    it "deletes an article" do
-      authorize "", "KEY"
-      delete "/articles/about"
-      last_response.status.must_equal 200
     end
   end
 end
