38faf20b20b478c83505f3c2a708a6beccae4656
Brandur <brandur@mutelight.org>
2012-12-28 12:13:32 -0800

    Fewer crazy Sinatraisms; more Ruby

--- lib/hekla/modules/assets.rb
+++ lib/hekla/modules/assets.rb
@@ -1,9 +1,8 @@
 module Hekla::Modules
   class Assets < Sinatra::Base
-    configure do
-      set :root, Hekla::Config.root
-      set :assets, (Sprockets::Environment.new { |env|
-        path = settings.root + "/themes/#{Hekla::Config.theme}/assets"
+    def initialize
+      path = "#{Hekla::Config.root}/themes/#{Hekla::Config.theme}/assets"
+      @assets = Sprockets::Environment.new do |env|
         Slides.log :assets, path: path
 
         env.append_path(path + "/images")
@@ -14,23 +13,23 @@ module Hekla::Modules
           env.js_compressor  = YUI::JavaScriptCompressor.new
           env.css_compressor = YUI::CssCompressor.new
         end
-      })
+      end
     end
 
     get "/assets/:release/app.css" do
       content_type("text/css")
-      respond_with_asset(settings.assets["app.css"])
+      respond_with_asset(@assets["app.css"])
     end
 
     get "/assets/:release/app.js" do
       content_type("application/javascript")
-      respond_with_asset(settings.assets["app.js"])
+      respond_with_asset(@assets["app.js"])
     end
 
     %w{jpg png}.each do |format|
       get "/assets/:image.#{format}" do |image|
         content_type("image/#{format}")
-        respond_with_asset(settings.assets["#{image}.#{format}"])
+        respond_with_asset(@assets["#{image}.#{format}"])
       end
     end
 
